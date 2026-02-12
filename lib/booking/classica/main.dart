afd2460ea6722144997c29ad45bff7d43ba53aed
Stefano Mini <info@stefanominiconsulting.com>
2026-02-12 18:11:27 +0200

    bg for classica

--- lib/booking/classica/main.dart
+++ lib/booking/classica/main.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:lapponia_travel_frontend/booking/repository.dart';
 
 class ClassicaMain extends StatelessWidget {
   const ClassicaMain({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: BookingHomeRepository().getImageUrl("uz2nqrg73ejg5ag"),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(child: Text("Errore nel caricare l'immagine")),
+          );
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: SizedBox(
+                height: 200,
+                width: 200,
+                child: CircularProgressIndicator.adaptive(),
+              ),
+            ),
+          );
+        } else if (snapshot.hasData) {
+          final image = snapshot.data!;
+          return Scaffold(
+            body: Container(
+              constraints: BoxConstraints.expand(),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(image),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: ClassicaContent(),
+            ),
+          );
+        } else {
+          return Scaffold(body: Center(child: Text("Nessun dato disponibile")));
+        }
+      },
+    );
+  }
+}
+
+class ClassicaContent extends StatelessWidget {
+  const ClassicaContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsGeometry.all(20),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black.withAlpha(100),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+      ),
+    );
   }
 }
